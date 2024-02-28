3209ca8967bb1bb459dd17d1b4aadde773bc559a
Victoria Ayala <139914452+vimayala@users.noreply.github.com>
2024-02-28 15:45:25 -0800

    Cant

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -16,6 +16,7 @@ class Title extends Phaser.Scene {
         this.load.image('minerva', 'img/minerva.png')
         this.load.image('jove', 'img/jove.png')
         this.load.image('neptune', 'img/neptune.png')
+        this.load.image('sammy', 'img/sammyslug.png')
 
         // load bitmap font
         this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml')
